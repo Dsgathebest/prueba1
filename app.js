762a9ad57adaf2bbb81a76c208c40f4b95542933
Dsgathebest <gomezabrildaniel1234@gmail.com>
2023-04-26 08:23:53 -0500

    Hice una modificacion

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 
+const getUser = ()=> {
+    return {
+        uid:'ABC123',
+        username :'El_Papi1502'
+    }
+};
+
+const getUser2 = ()=>({
+    uid:'ABC123',
+    username:'El_Papi1502'
+});
+console.log(getUser2());
+
+const user = getUser();
+console.log (user);
